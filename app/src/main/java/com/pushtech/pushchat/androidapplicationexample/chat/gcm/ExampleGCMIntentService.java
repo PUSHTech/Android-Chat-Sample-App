a9ed8d801c61bca12cde4d5b9ed350cb9fbaa22b
goda87 <goda87@gmail.com>
2014-09-29 18:27:28 +0200

    put app icon in notifications from campaigns

--- app/src/main/java/com/pushtech/pushchat/androidapplicationexample/chat/gcm/ExampleGCMIntentService.java
+++ app/src/main/java/com/pushtech/pushchat/androidapplicationexample/chat/gcm/ExampleGCMIntentService.java
@@ -1,8 +1,15 @@
 package com.pushtech.pushchat.androidapplicationexample.chat.gcm;
 
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.pushtech.pushchat.androidapplicationexample.R;
 import com.pushtech.pushchat.androidapplicationexample.chat.chatscreens.ChatListActivity;
+import com.pushtech.pushchat.androidapplicationexample.chat.notifications.NotificationManager;
 import com.pushtech.sdk.chat.gcm.GCMEventIntentService;
 import com.pushtech.sdk.chat.service.CommunicationService;
+import com.pushtech.sdk.model.CampaignDelivery;
 
 /**
  *
@@ -16,7 +23,19 @@ public final class ExampleGCMIntentService extends GCMEventIntentService {
 
     @Override
     protected CommunicationService.ChatEventsListener getCommunicationServiceListener() {
-        return null;
-//        return NotificationManager.with(this);
+        return NotificationManager.with(this);
+    }
+
+    @Override
+    protected void onCampaignDeliveryDataReceived(Context context, CampaignDelivery pushDelivery) {
+
+        Log.d("GODA", "EIIEIE " + pushDelivery.getText());
+        String text = pushDelivery.getText();
+        if (!TextUtils.isEmpty(pushDelivery.getUrl())) {
+            text += " " + pushDelivery.getUrl();
+        }
+
+        notify(this, pushDelivery.getTitle(), text,
+                pushDelivery.getUrl(), R.drawable.app_launcher);
     }
 }
